193fd4d19a6a520e7a63908299213de8e8d29a5c
Andrew Yoo <andrewyoo1@proton.me>
2025-08-04 07:53:13 -0400

    Finished Argon2 Hash function

---
+++ key_derivation.py
@@ -0,0 +1,41 @@
+import secrets
+from argon2 import PasswordHasher
+
+# Settings
+argon_time_cost = [3, 4, 8]
+argon_memory_cost = [65_536, 1_048_576, 1_048_576]
+argon_parallelism = [4, 4, 8]
+argon_hash_length = [32, 32, 32]
+argon_encoding = 'utf-8'
+
+def derive_key(hash_length: int, password: bytes, salt_length: int, mode: int):
+    """
+    Use Argon2 to derive a key from a password.
+    
+    Args:
+        hash_length (int): The length of the hashed key, in bytes.
+        password (bytes): The password.
+        salt_length (int): The length of the salt, in bytes.
+        mode (int): 0-light, 1-normal, 2-overkill
+    """
+
+    # Light Mode
+    if mode == 0:
+        argon_salt = secrets.token_bytes(salt_length)
+        ph = PasswordHasher(time_cost=argon_time_cost[0], memory_cost=argon_memory_cost[0], parallelism=argon_parallelism[0], hash_len=argon_hash_length[0], salt_len=salt_length, encoding=argon_encoding)
+        hash = ph.hash(password=password, salt=argon_salt)
+        return hash
+    
+    # Normal Mode
+    elif mode == 1:
+        argon_salt = secrets.token_bytes(salt_length)
+        ph = PasswordHasher(time_cost=argon_time_cost[1], memory_cost=argon_memory_cost[1], parallelism=argon_parallelism[1], hash_len=argon_hash_length[1], salt_len=salt_length, encoding=argon_encoding)
+        hash = ph.hash(password=password, salt=argon_salt)
+        return hash
+
+    # Overkill Mode
+    elif mode == 2:
+        argon_salt = secrets.token_bytes(salt_length)
+        ph = PasswordHasher(time_cost=argon_time_cost[2], memory_cost=argon_memory_cost[2], parallelism=argon_parallelism[2], hash_len=argon_hash_length[2], salt_len=salt_length, encoding=argon_encoding)
+        hash = ph.hash(password=password, salt=argon_salt)
+        return hash
---
+++ protocol.md
@@ -0,0 +1,9 @@
+# Protocol Details
+
+## Cryptography
+
+- Normal
+    - Argon2id
+        - 4 operations
+        - 1 GiB Ram
+        - 4 Threads
