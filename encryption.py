d1a3d6414ce44ca307c3af8463ba783b5a3d3d9f
Andrew Yoo <andrewyoo1@proton.me>
2025-08-05 10:28:31 -0400

    Rewrote cascade encryption to wrap cryptography module.

--- encryption.py
+++ encryption.py
@@ -1,5 +1,5 @@
 import secrets
-from Crypto.Cipher import ChaCha20, AES
+from Crypto.Cipher import ChaCha20
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
@@ -25,22 +25,20 @@ def xchacha_camellia_aes(xchacha_key: bytes, camellia_key: bytes, aes_key: bytes
     Use XChaCha20, AES-256-CTR, and Serpent-256-CTR cascaded to encrypt data.
     """
     # XChaCha20
-
     xchacha_nonce = secrets.token_bytes(24)
     xchacha_cipher = ChaCha20.new(key=xchacha_key, nonce=xchacha_nonce)
     xchacha_ciphertext = xchacha_cipher.encrypt(plaintext)
 
     # Camellia-CTR
-
     camellia_nonce = secrets.token_bytes(16)
     camellia_cipher = Cipher(algorithm=algorithms.Camellia(key=camellia_key), mode=modes.CTR(camellia_nonce), backend=default_backend)
     camellia_encryptor = camellia_cipher.encryptor()
     camellia_ciphertext = camellia_encryptor.update(xchacha_ciphertext) + camellia_encryptor.finalize()
 
     # AES-256-CTR
-
-    aes_nonce = secrets.token_bytes(15)
-    aes_cipher = AES.new(key=aes_key, mode=AES.MODE_CTR, nonce=aes_nonce)
-    aes_ciphertext = aes_cipher.encrypt(camellia_ciphertext)
+    aes_nonce = secrets.token_bytes(16)
+    aes_cipher = Cipher(algorithm=algorithms.AES256(key=aes_key), mode=modes.CTR(aes_nonce), backend=default_backend)
+    aes_encryptor = aes_cipher.encryptor()
+    aes_ciphertext = aes_encryptor.update(camellia_ciphertext) + aes_encryptor.finalize()
 
     return xchacha_nonce, camellia_nonce, aes_nonce, aes_ciphertext
